16b9ce82c794cdd414ad7ee237da080854bb4ea8
Thomas-Chiu <josemich0511@gmail.com>
2021-12-02 00:02:37 +0800

    update(App.vue): 新增匯出 CSV 檔

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <ag-grid-vue
-    style="width: 75%; height: 95%"
-    class="ag-theme-alpine"
-    rowSelection="multiple"
-    :columnDefs="columnDefs"
-    :rowData="rowData"
-  >
-  </ag-grid-vue>
+  <div id="app" class="bg-dark">
+    <div class="container h-100 d-flex flex-column">
+      <h1 class="text-white">
+        運輸資料流通服務平臺 (Transport Data eXchange, TDX)
+      </h1>
+      <b-button-group>
+        <b-button variant="outline-warning" @click="onBtnExport"
+          >匯出 CSV 檔</b-button
+        >
+        <b-button variant="outline-warning">Button</b-button>
+        <b-button variant="outline-warning">Button</b-button>
+        <b-button variant="outline-warning">Button</b-button>
+      </b-button-group>
+      <ag-grid-vue
+        style="width: 100%; height: 100%"
+        rowSelection="multiple"
+        class="ag-theme-alpine"
+        :rowDragManaged="true"
+        :animateRows="true"
+        :suppressExcelExport="true"
+        :columnDefs="columnDefs"
+        :rowData="rowData"
+        @grid-ready="onGridReady"
+      >
+      </ag-grid-vue>
+    </div>
+  </div>
 </template>
 
 <script>
 import { AgGridVue } from "ag-grid-vue";
-import axios from "axios";
 import colomnDefs from "./assets/colomnDefs.json";
 import getData from "./assets/tdxApi.js";
 
@@ -24,7 +42,20 @@ export default {
     return {
       columnDefs: [],
       rowData: [],
+      // gridApi: null,
+      // columnApi: null,
     };
+  },
+  methods: {
+    onGridReady(params) {
+      console.log(params);
+      this.gridApi = params.api;
+      this.columnApi = params.columnApi;
+      this.gridApi.sizeColumnsToFit();
+    },
+    onBtnExport() {
+      this.gridApi.exportDataAsCsv();
+    },
   },
   beforeMount() {
     this.columnDefs = colomnDefs.data;
@@ -48,8 +79,6 @@ export default {
       .catch((err) => {
         console.log(err);
       });
-
-    // rowData=
   },
 };
 </script>
@@ -58,11 +87,15 @@ export default {
 @import "~ag-grid-community/dist/styles/ag-grid.css";
 @import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-body {
+#app {
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 0;
+
+  button {
+    margin: 10px 0px;
+  }
 }
 </style>
